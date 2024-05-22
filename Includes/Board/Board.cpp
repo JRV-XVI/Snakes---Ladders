3ca38845bb0522e91edddb70449ee30ac7fa23c5
TheShadowW0LF16 <jrvmzo@hotmail.com>
2024-05-21 21:33:00 -0600

    Board functionality
    Class board created

--- Includes/Board/Board.cpp
+++ Includes/Board/Board.cpp
@@ -2,5 +2,34 @@
 
 Board::Board()
 {
-    
+    int stateTile = 0; // Switch to change a snake/ladder tile
+    for(int index = 1; index <= 30; index++) {
+        // Snake tile
+        if(index % 7 == 0 && stateTile != 0 && snakes > 0) {
+            Table[index] = 'S';
+            stateTile = 0;
+            snakes -= 1;
+        }
+        // Ladder tile
+        else if(index % 8 == 0 && stateTile == 0 && ladders > 0)
+        {
+            Table[index] = 'L';
+            stateTile = 1;
+            ladders -= 1;
+        }
+        // Empty tile
+        else {
+            Table[index] = 'N';
+        }
+    }
+}
+
+void Board::getMatrix()
+{
+    // Prints the type of the element on the table by index
+    for(int i = 1; i <= 30; i++)
+    {
+        cout << Table[i] << endl;
+    }
+
 }
--- Includes/Board/Board.h
+++ Includes/Board/Board.h
@@ -1,13 +1,19 @@
 #ifndef BOARD
 #define BOARD
 
+#include "iostream"
+using namespace std;
+
 class Board
 {
 private:
     /* data */
-    int Table[30];
+    int snakes = 3; // Limits of snake tiles
+    int ladders = 3; // Limits of ladder tiles
 public:
-    Board(/* args */);
+    char Table[30]; // Table of 30 tiles
+    Board(); // Constructor
+    void getMatrix(); // Method to check the setting of table (snake, ladder, empty)
 };
 
 #endif
