6d4633a976a986839c01183e83e389f149e7f2f8
JoseAlfredoV <vergaraperaltajosealfredo@gmail.com>
2024-05-22 20:02:12 -0600

    main game loop beta
    all requirements complete, testing and final adjustments might be needed

--- Includes/Game/MyGame.cpp
+++ Includes/Game/MyGame.cpp
@@ -48,35 +48,75 @@ int MyGame::mainloop(){
 
   Board board; //Build board
   Dice dice; // create and roll Dice
+
+  int steps; // position after tile effects
+  int num; // Number on wich the dice land on
+
   board.getMatrix();  //Get the tiles that make up the board
 
   while (1){
     //To get the record of which turn is
     
+    
     //Player 1 turn
     MyGame::handlopt(); //If the option is not continue it will end the program
 
-    int num=dice.roll(); //Roll the Dice
+    num=dice.roll(); //Roll the Dice
+    // apply tile effects
+    if (P1.getPosition()+num<29)
+    {
+    if (board.Table[P1.getPosition()+num-1]=='S'){
+      steps=P1.getPosition()+num-3;
+    }
+    else if (board.Table[P1.getPosition()+num-1]=='L')
+    {
+      steps=P1.getPosition()+num+3;
+    }
+    else{
+      steps=P1.getPosition()+num;
+    }
     
     //Needed info for Player 1
     cout << "Turn: " << turn << endl;
     cout <<"Player ID: "<<P1.getId()<<endl<<"Position: "<<P1.getPosition()<<endl; // player ID and Position
     cout <<"Number Rolled:  "<< num<<endl; // number rolled
-
-    P1.setPosition(P1.getPosition()+num); //set new Player position
+    cout <<"Tile type: "<< board.Table[P2.getPosition()+num-1]<<endl; // type of tile landed on
+    P1.setPosition(steps); //set new Player position
     cout <<"New position: "<< P1.getPosition()<<endl;
-
+    }
+    else{
+      cout << "Player 1 rolled a "<<num<< "a won" << endl;
+      exit(0);
+    }
+    
     //Needed info for Player 2
     MyGame::handlopt(); //If the option is not continue it will end the program
-
+    if (P2.getPosition()+num<29)
+    {
     num=dice.roll(); //Roll the Dice
-    
+    // apply tile efects
+    if (board.Table[P2.getPosition()+num-1]=='S'){
+      steps=P2.getPosition()+num-3;
+    }
+    else if (board.Table[P2.getPosition()+num-1]=='L')
+    {
+      steps=P2.getPosition()+num+3;
+    }
+    else{
+      steps=P2.getPosition()+num;
+    }
+
     //Needed info for Player 2
     cout << "Turn: " << turn << endl;
     cout <<"Player ID: "<<P2.getId()<<endl<<"Position: "<<P2.getPosition()<<endl; // player ID and Position
-    cout <<"Number Rolled:  "<< num<<endl; // number rolled
-
-    P2.setPosition(P2.getPosition()+num); //set new Player position
+    cout <<"Number Rolled: "<< num<<endl; // number rolled
+    cout <<"Tile type: "<< board.Table[P2.getPosition()+num-1]<<endl; // type of tile landed on
+    P2.setPosition(steps); //set new Player position
     cout <<"New position: "<< P2.getPosition()<<endl;
+    }
+    else{
+      cout << "Player 2 rolled a "<<num<< "a won" << endl;
+      exit(0);
+    }
   }
 }
