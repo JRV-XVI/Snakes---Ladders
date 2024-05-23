082d04f1dcf118e4d8d2cc9213fb560796ea9073
AndreyBal03 <andreybalvaneda2@gmail.com>
2024-05-23 09:34:59 -0600

    Corret indexing Ladders and snakes

--- Includes/Board/Board.cpp
+++ Includes/Board/Board.cpp
@@ -4,7 +4,6 @@
 
 int random(int v1, int v2);
 bool srand_called = false;
-bool in(int val, int* arr);
 
 Board::Board(){
   int lpos[ladders]; // Ladders positions
@@ -18,10 +17,9 @@ Board::Board(){
   for (int j = 0; j<ladders; j++){
     n_ = random(7,26); //Makes a random selection of tiles 
 
-      while(in(n_, lpos)){  //Checks if that position is free
+      while(Table[n_] != 'N'){  //Checks if that position is free
         n_ = random(7,26);
       }
-
     Table[n_] = 'L'; //Change the value of the tile
     lpos[j] = n_; //Storage the new ladder position
   }
@@ -30,26 +28,13 @@ Board::Board(){
   for (int j = 0; j<snakes; j++){
     n_ = random(7,26);
 
-    while(in(n_, spos) || in(n_, lpos) ){
+    while(Table[n_] != 'N'){
       n_ = random(7,26);
     }
+    cout << n_ << endl;
     Table[n_] = 'S';
     spos[j] = n_;
   }
-}
-
-
-bool in(int val, int* arr){
-  //This function check if a value is inside of an array
-  int ans;
-  int l = sizeof(arr)/sizeof(int);
-
-  for(int i = 0; i<l; i++){
-    if (val == arr[i]){
-      return true;
-    }
-  }
-  return false;
 }
 
 int random(int v1, int v2) {
