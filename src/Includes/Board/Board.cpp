bb81ff655ab9011fbf31ad077a0f6c94a8d3ad01
AndreyBal03 <andreybalvaneda2@gmail.com>
2024-06-01 18:28:19 -0600

    New Board

--- src/Includes/Board/Board.cpp
+++ src/Includes/Board/Board.cpp
@@ -1,34 +1,94 @@
 #include "Board.h"
 #include <cstdlib>
 #include <ctime>
+#include <vector>
+
+#include "../Square/Square.h"
+#include "../Square/Snake.h"
+#include "../Square/Ladder.h"
 
 int random(int v1, int v2);
 bool srand_called = false;
 
-Board::Board(){
-  int n_;
 
-  for (int i = 0; i<30; i++){
-    Table[i] = 'N'; //Define all tiles to a N
+Board::Board(){
+  setBoard();
+}
+
+Board::Board(int newSize, int newSnakes, int newLadders,
+        int newLadderEffect, int newSnakeEffect){
+
+  size = newSize;
+  snakes = newSnakes;
+  ladders = newLadders;
+  ladderEffect = newLadderEffect;
+  snakeEffect = newSnakeEffect;
+
+  setBoard();
+}
+
+void Board::setBoard(){
+
+  int n_;
+  int r1, r2; //This will be the ranges of ladders and square appear
+  r1 = - snakeEffect; //Snake effect is negative so (-)(-) = +
+  r2 = size - ladderEffect;
+
+  Square *iter;
+
+  for (int i = 0; i<size; i++){
+    Table.push_back(new Square); //Define all tiles to be the basic square
   }
 
   for (int j = 0; j<ladders; j++){
-    n_ = random(7,26); //Makes a random selection of tiles 
+    n_ = random(1,r2); //Makes a random selection of tiles 
+    iter = Table[n_];
 
-      while(Table[n_] != 'N'){  //Checks if that position is free
-        n_ = random(7,26);
-      }
-    Table[n_] = 'L'; //Change the value of the tile
+    while(iter->getSymbol() != 'N'){  //Checks if that position is free
+      n_ = random(1,r2);
+      iter = Table[n_];
+    }
+
+    Table[n_] = new Ladder(ladderEffect);
   }
 
   //Repeate for snakes
   for (int j = 0; j<snakes; j++){
-    n_ = random(7,26);
+    n_ = random(r1,size);
+    iter = Table[n_];
 
-    while(Table[n_] != 'N'){
-      n_ = random(7,26);
+    //This will try to find a valid index that is not used
+    //and it does not imply a circular path
+    while(iter->getSymbol() != 'N' || checkCircularPath(n_)){
+      n_ = random(r1, size);
+      iter = Table[n_];
     }
-    Table[n_] = 'S';
+
+    Table[n_] = new Snake(snakeEffect); //Change the value of the tile
+  }
+}
+
+bool Board::checkCircularPath(int index){
+  vector <int> indexs; //array of all index passed
+  int eff;
+  Square *iter_; //sets a iterator
+
+  while(1){
+    indexs.push_back(index); //Add the last index we were
+    iter_ = Table[index]; //Gets the item
+    eff = iter_->getEffect(); //Gets the effect
+
+    index += eff; //Gets the new index
+
+    //If the new index is tha same of previous return false
+    //there is no a circular path
+    if (index == indexs[indexs.size()-1]) return false;
+
+    //Else check if the new index were before in our path
+    for (int i = 0; i < indexs.size() - 1; i++){
+      //If yes, return true, there is a circular path
+      if (index == indexs[i]) return true;
+    }
   }
 }
 
@@ -41,3 +101,4 @@ int random(int v1, int v2) {
     int n = v2 - v1;
     return (rand() % (n + 1)) + v1;
 }
+
