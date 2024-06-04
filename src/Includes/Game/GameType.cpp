983979b3a885de2c1b73e8a4ad4447bafdae0e24
TheShadowW0LF16 <jrvmzo@hotmail.com>
2024-06-03 21:48:33 -0600

    Fixed checkTurn

--- src/Includes/Game/GameType.cpp
+++ src/Includes/Game/GameType.cpp
@@ -16,60 +16,32 @@ GameType::GameType() {
     cout << board << endl;  //Get the tiles that make up the board
 
     while (true) {
-        //To get the record of which turn is
-        turn++;
-        //Player 1 turn
-        GameType::handlopt(); //If the option is not continue it will end the program
+        for (int index = 0; index < 2; index++) {
+            turn++;
+            GameType::handlopt(); //If the option is not continue it will end the program
+            cout << endl;
+            num = dice.roll(); //Roll the Dice
+            // apply tile effects
+            if (playerList.getPosition(index) + num >= 30){
+            cout << "Player " << playerList.getId(index) << " rolled "<< num << " and won" << endl;
+            exit(0);
+            }
 
-        num = dice.roll(); //Roll the Dice
-        // apply tile effects
-        if (playerList.getPosition(0) + num >= 30){
-        cout << "Player 1 rolled "<< num << " and won" << endl;
-        exit(0);
+            iter = board.Table[playerList.getPosition(index) + num - 1];
+
+            //This line makes the update of the position
+            steps = playerList.getPosition(index) + num + iter->getEffect();
+
+            //Needed info for Player 1
+            cout << "Turn: " << turn << endl;
+            cout << "Player ID: "<< playerList.getId(index) << endl;
+            cout << "Position: " << playerList.getPosition(index) << endl; // player ID and Position
+            cout << "Number Rolled: " << num << endl; // number rolled
+            cout << "Tile type: " << iter -> getSymbol() << endl; // type of tile landed on
+            playerList.setPosition(index, steps); //set new Player position
+            cout << "New position: "<< playerList.getPosition(index) << endl;
+            GameType::checkTurn();//Checking for max turn
         }
-
-        iter = board.Table[playerList.getPosition(0) + num - 1];
-
-        //This line makes the update of the position
-        steps = playerList.getPosition(0) + num + iter->getEffect();
-
-        //Needed info for Player 1
-        cout << "Turn: " << turn << endl;
-        cout << "Player ID: "<< playerList.getId(0) << endl;
-        cout << "Position: " << playerList.getPosition(0) << endl; // player ID and Position
-        cout << "Number Rolled: " << num << endl; // number rolled
-        cout << "Tile type: " << iter -> getSymbol() << endl; // type of tile landed on
-        playerList.setPosition(0, steps); //set new Player position
-        cout << "New position: "<< playerList.getPosition(0) << endl;
-        GameType::checkTurn();//Checking for max turn
-
-        cout << endl;
-        turn++;
-        //Needed info for Player 2
-        GameType::handlopt(); //If the option is not continue it will end the program 
-        num = dice.roll(); //Roll the Dice
-
-        if (playerList.getPosition(1) + num >= 30){
-        cout << "Player 2 rolled "<< num << " and won" << endl;
-        exit(0);
-        }
-
-        // apply tile efects
-        iter = board.Table[playerList.getPosition(1) + num - 1];
-
-        //Update the steps of the player
-        steps = playerList.getPosition(1) + num + iter->getEffect();
-
-        //Needed info for Player 2
-        cout << "Turn: " << turn << endl;
-        cout << "Player ID: "<< playerList.getId(1) << endl;
-        cout << "Position: "<< playerList.getPosition(1) << endl; // player ID and Position
-        cout << "Number Rolled: "<< num <<endl; // number rolled
-        cout << "Tile type: "<< iter ->getSymbol() << endl; // type of tile landed on
-        playerList.setPosition(1, steps); //set new Player position
-        cout << "New position: " << playerList.getPosition(1) << endl;
-        cout << endl;
-        GameType::checkTurn();//Checking for max turn
     }
 }
 
@@ -217,10 +189,9 @@ void GameType::checkTurn(){
         for (int i=1;i<playerList.getPlayerCount();i++){
             if (playerList.getPosition(winnerByDefault)<playerList.getPosition(i)){
                 winnerByDefault=i;
-
-            cout<<"The max turn limit has been reached, Player "<<winnerByDefault+1<<" has won by default with a total of "<<playerList.getPosition(winnerByDefault)<<" tiles, got the max distance first"<<endl;
-            exit(0);
             }
+        cout<<"The max turn limit has been reached, Player "<<winnerByDefault+1<<" has won by default with a total of "<<playerList.getPosition(winnerByDefault)<<" tiles, got the max distance first"<<endl;
+        exit(0);
         }
         
 }
